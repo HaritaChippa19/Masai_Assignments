6f223f0c0e631647af42695fcb40d6ef8d72290c
HaritaChippa19 <chippaharita668@gmail.com>
2025-11-23 17:26:26 +0530

    Question12 completed

---
+++ ES6-Assesment/index.js
@@ -0,0 +1,12 @@
+//Question 11
+const multiply = (a = 1, b = 1) => a * b;
+console.log(multiply(3, 4)); 
+console.log(multiply());
+
+//Question 12
+const calculate = n => ({ 
+    square: n * n, cube: n * n * n 
+});
+console.log(calculate(5));
+
+
