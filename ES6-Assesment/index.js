cf3a33cd35028b79217d1a695940e68527f74c7a
HaritaChippa19 <chippaharita668@gmail.com>
2025-11-23 17:35:09 +0530

    Question13 completed

--- ES6-Assesment/index.js
+++ ES6-Assesment/index.js
@@ -9,4 +9,27 @@ const calculate = n => ({
 });
 console.log(calculate(5));
 
+//Question 13
+const people = [ 
+    { 
+        name: "Alice", 
+        address: 
+        { city: "New York", street: 
+            { name: "Broadway", number: 123 } 
+        } 
+    }, 
+    { name: "Bob", address: 
+        { city: "Los Angeles", street: 
+            { name: "Sunset Boulevard", number: 456 } 
+        } 
+    } 
+];
+const result = [];
 
+for (const { name, address: { city, street: { name: streetName } } } of people) {
+  result.push(`${name} lives in ${city} on ${streetName}`);
+}
+
+console.log(result);
+
+//Question 14
