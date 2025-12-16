69f638faf970b684ffee331a175a3f508f74768f
Harita <chippaharita668@gmail.com>
2025-12-16 16:24:33 +0530

    Multi Page component completed

---
+++ MultiComponentPage/src/Content.jsx
@@ -0,0 +1,7 @@
+function Content() {
+  return (
+    <h3>This is Main Content</h3>
+  );
+}
+
+export default Content;
---
+++ MultiComponentPage/src/Navbar.jsx
@@ -0,0 +1,7 @@
+function Navbar() {
+  return (
+    <h3>This is Navbar</h3>
+  );
+}
+
+export default Navbar;
