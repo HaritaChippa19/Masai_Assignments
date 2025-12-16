31319c14a200f534c66154c1b8eeaeb90c44b6ca
Harita <chippaharita668@gmail.com>
2025-12-16 18:10:47 +0530

    Question 4 completed

---
+++ StateManagement/Question4/src/App.jsx
@@ -0,0 +1,9 @@
+import UserProfile from "./UserProfile";
+
+function App() {
+  return (
+    <UserProfile />
+  );
+}
+
+export default App;
---
+++ StateManagement/Question4/src/UserInfo.jsx
@@ -0,0 +1,11 @@
+function UserInfo(props) {
+  return (
+    <>
+      <h3>User Information</h3>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+    </>
+  );
+}
+
+export default UserInfo;
